44e19f62f491c163868f27efc2f2d6d0481c2726
Rob Young <rob@robyoung.digital>
2017-10-14 10:31:24 +0100

    Add URL processor for handling models

--- mind/main/blueprint.py
+++ mind/main/blueprint.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for, request
+from flask import Blueprint, render_template, redirect, url_for, request, abort
 
 from mind.models import Question, Answer
 from mind.app import db
@@ -9,8 +9,7 @@ main = Blueprint("main", __name__)
 @main.route("/")
 def index():
     question_slug = 'how-are-you-feeling'
-    question_url = url_for(
-        '.show_question', question_slug=question_slug)
+    question_url = url_for('.show_question', question=question_slug)
     return redirect(question_url), 301
 
 
@@ -31,28 +30,40 @@ def add_question():
     return redirect(url_for(".list_questions"))
 
 
-@main.route("/question/<question_slug>", methods=["GET"])
-def show_question(question_slug):
-    question = Question.query.filter_by(slug=question_slug).one_or_none()
-    if not question:
-        # TODO: proper 404
-        return "", 404
-
-    return render_template(
-        "show_question.html",
-        question=question)
+@main.route("/question/<question>", methods=["GET"])
+def show_question(question):
+    return render_template("show_question.html", question=question)
 
 
-@main.route("/question/<question_slug>/answer", methods=["POST"])
-def add_answer(question_slug):
+@main.route("/question/<question>/answer", methods=["POST"])
+def add_answer(question):
     # TODO: add flash message
-    # TODO: move to route filter
-    question = Question.query.filter_by(slug=question_slug).one_or_none()
-    if not question:
-        # TODO: proper 404
-        return "", 404
     answer = Answer(question_id=question.id, answer=request.form['answer'])
     db.session.add(answer)
     db.session.commit()
 
-    return redirect(url_for(".show_question", question_slug=question_slug))
+    return redirect(url_for(".show_question", question=question))
+
+
+MODEL_URL_MAP = {
+    'question': Question
+}
+
+
+@main.url_defaults
+def add_slug_to_url(endpoint, values):
+    for field in MODEL_URL_MAP.keys():
+        if field in values and hasattr(values[field], 'slug'):
+            values[field] = values[field].slug
+
+
+@main.url_value_preprocessor
+def resolve_slug(endpoint, values):
+    for field, model in MODEL_URL_MAP.items():
+        if field in values:
+            record = model.query \
+                    .filter_by(slug=values[field]) \
+                    .one_or_none()
+            if not record:
+                abort(404)
+            values[field] = record
